539441355a7a1c85563478875db3d15f10f82ce0
nelson <nelsonlinux9@gmail.com>
2017-11-20 19:15:41 +0800

    update

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 const   express     = require('express');
 const   bodyParser  = require('body-parser');
+const   path        = require('path');
 const   app         = express();
 const   {router}    = require('./routes/todos.js');
 
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'views')));
 
 
 
 
 app.use('/api/todos', router)
 app.get('/', (req,res)=>{
-    res.send('<h1>Hi there from root Route</h1>');
+    res.sendFile('index.html')
 });
 
 
